e89235696746d8e4efc3325c8927dc3abce36310
Francisco Emerson Souza Reinaldo <emersonreinaldo234@gmail.com>
2026-03-30 09:38:37 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-const botao = document.querySelector("#btnEnviar");
-botao.addEventListener("click", function(){
-    alert("Botão clicado!");
+const botaoEnviar = document.querySelector("#btnEnviar");
+const botaoExcluir = document.querySelector("#btnExcluir");
+const caixa = document.querySelector(".caixa");
+
+botaoEnviar.addEventListener("click", function () {
+
+    botaoEnviar.style.position = "absolute";
+
+    const largura = caixa.clientWidth - botaoEnviar.clientWidth;
+    const altura = caixa.clientHeight - botaoEnviar.clientHeight;
+
+    const x = Math.random() * largura;
+    const y = Math.random() * altura;
+
+    botaoEnviar.style.left = x + "px";
+    botaoEnviar.style.top = y + "px";
+});
+
+botaoExcluir.addEventListener("click", function () {
+    location.reload();
 });
