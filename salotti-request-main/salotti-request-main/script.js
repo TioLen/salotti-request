43ca2b254fdd622e42a069b08078f1698ecec738
TioLen <lenwaterson13@gmail.com>
2026-04-13 08:35:05 -0300

    limpeza de arquivos não utilizados

--- salotti-request-main/salotti-request-main/script.js
+++
@@ -1,6 +0,0 @@
-const botao = document.querySelector("#btnEnviar");
-
-botao.addEventListener("click", function() {
-    console.log("%c Transmissão Iniciada...", "color: #00f2ff; font-weight: bold;");
-    alert("Acesso autorizado. Sistema carregando...");
-});
